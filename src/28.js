b8039572ff762e3df465c4cb1f49637f543f9812
IsaacATetteh <iabbeytetteh@gmail.com>
2023-09-30 06:09:55 +0100

    Leetcode 28

---
+++ src/28.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if(haystack.includes(needle)) {
+        return haystack.indexOf(needle);
+    } else {
+        return -1;
+    }
+};
