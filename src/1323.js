9c07c8e0acfcb0ed90f14c89313bf8fdde9c7732
IsaacATetteh <iabbeytetteh@gmail.com>
2023-10-08 20:22:55 +0100

    Leetcode 1323

--- src/1323.js
+++ src/1323.js
@@ -2,13 +2,13 @@
  * @param {number} num
  * @return {number}
  */
-var maximum69Number  = function(num) {
-    let str = num.toString();
-    if(str.includes("6")) {
-    let pos = str.indexOf("6")
+var maximum69Number = function (num) {
+  let str = num.toString();
+  if (str.includes("6")) {
+    let pos = str.indexOf("6");
     str = str.replace("6", "9");
     return str;
-    } else {
-        return num;
-    }
+  } else {
+    return num;
+  }
 };
