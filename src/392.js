580055714997d653a563a01ee17f81c9ec5e0bb1
IsaacATetteh <iabbeytetteh@gmail.com>
2023-10-16 20:53:06 +0100

    Leetcode 392

---
+++ src/392.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  let j = 0;
+
+  if (s.length < 1) {
+    return true;
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    if (t[i] === s[j]) {
+      j++;
+      if (j == s.length) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
