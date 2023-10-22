0eb73be9b725f0cdc8185933b5aa14a4d66c13d9
IsaacATetteh <iabbeytetteh@gmail.com>
2023-10-22 16:10:45 +0100

    Leetcode 167

---
+++ src/167.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    if (numbers[left] + numbers[right] < target) {
+      left++;
+    } else if (numbers[left] + numbers[right] > target) {
+      right--;
+    } else {
+      return [left + 1, right + 1];
+    }
+  }
+};
