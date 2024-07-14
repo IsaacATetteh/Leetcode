92ff5a10c0344fd670c6ad32c1166c5f79355645
Isaac <iabbeytetteh@gmail.com>
2024-07-14 23:38:33 +0100

    Leetcode 3110

---
+++ src/3110.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function (s) {
+  let result = 0;
+
+  for (let i = 0; i < s.length - 1; i++) {
+    result += Math.abs(s[i].charCodeAt() - s[i + 1].charCodeAt());
+  }
+  return result;
+};
